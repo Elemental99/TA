117b6acdfc41210f6c303fee05f27faeb4911266
Kevin PM <kevinponcesite@gmail.com>
2022-06-24 23:32:50 -0500

    arreglo de pequeños errores

--- Sitio Web/client/src/app/pages/bares/index-bar/index-bar.component.ts
+++ Sitio Web/client/src/app/pages/bares/index-bar/index-bar.component.ts
@@ -1,43 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { response } from 'express';
-import { BarService } from 'src/app/services/bar.service';
-
+import { Component, OnInit } from '@angular/core'
+import { BarService } from 'src/app/services/bar.service'
 
 @Component({
-  selector: 'app-index-bar',
-  templateUrl: './index-bar.component.html',
-  styleUrls: ['./index-bar.component.css']
+    selector   : 'app-index-bar',
+    templateUrl: './index-bar.component.html',
+    styleUrls  : ['./index-bar.component.css']
 })
 export class IndexBarComponent implements OnInit {
-  public bares: any;
-  // public filtrotext: any;
+    public bares: any
 
+    // public filtrotext: any;
 
-  constructor(
-    private barService: BarService
-  ) { }
+    constructor(
+        private barService: BarService
+    ) { }
 
-  ngOnInit(): void {
-    this.barService.consultar_bares().subscribe(
-      response =>{ 
-        this.bares = response.bares
-        
-        console.log(this.bares);
-          
-      },
-      error =>{
+    ngOnInit(): void {
+        this.consultar_bares()
+    }
 
-      }
-    )
+    // buscar(buscarForm: {value:{filtro:any}}){
+    //   this.barService.consultar_bares(buscarForm.value.filtro).subscribe(
+    //     response=>{
+    //       this.bares = response.bares
+    //     }
+    //   )
+    // }
 
-  }
-
-  // buscar(buscarForm: {value:{filtro:any}}){
-  //   this.barService.consultar_bares(buscarForm.value.filtro).subscribe(
-  //     response=>{
-  //       this.bares = response.bares
-  //     }
-  //   )
-  // }
+    consultar_bares(): void {
+        this.barService.consultar_bares().subscribe(
+            response => {
+                this.bares = response.bares
+            },
+            error => {
+                console.error(error)
+            }
+        )
+    }
 
 }
--- Sitio Web/client/src/app/services/bar.service.ts
+++ Sitio Web/client/src/app/services/bar.service.ts
@@ -1,23 +1,26 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core'
+import { environment } from 'src/environments/environment'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs'
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class BarService {
-  public ruta;
-  constructor(
-    private http:HttpClient
-  ) { 
-    this.ruta = environment.API_URL;
-  }
+    public ruta
 
-  consultar_bares(): Observable <any> {
-    const headers = new HttpHeaders().set('Content-Type', 'application/json');
-    return this.http.get(this.ruta + '/', {headers:headers})
-  } 
+    constructor(
+        private http: HttpClient
+    ) {
+        this.ruta = environment.API_URL
+    }
 
+    consultar_bares(): Observable<any> {
+        const headers = new HttpHeaders().set(
+            'Content-Type',
+            'application/json'
+        )
+        return this.http.get(this.ruta + '/bar', { headers: headers })
+    }
 
 }
