1f9f12d04b6947849bb3449fa40a62ddc9f6dfa6
Kevin PM <kevinponcesite@gmail.com>
2022-06-29 22:42:35 -0500

    eslint agragacion

---
+++ Sitio Web/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    settings: {
+        'eslint.workingDirectories': ['./client', './server'],
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+    },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'standard',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    rules: {
+        indent: ['error', 4],
+        'space-before-function-paren': [
+            'error',
+            {
+                anonymous: 'never',
+                named: 'never',
+                asyncArrow: 'never',
+            },
+        ],
+        'comma-dangle': ['error', 'always-multiline'],
+        'no-useless-constructor': 'off',
+    },
+}
